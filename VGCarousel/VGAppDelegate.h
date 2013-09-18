df905fd65120d1985fe7df8668fb8ac1b68b1f8d
Charles Joseph Uy <charles@voyagerinnovation.com>
2013-09-18 15:39:42 +0800

    Initial Commit

---
+++ VGCarousel/VGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VGAppDelegate.h
+//  VGCarousel
+//
+//  Created by Charles Joseph Uy on 9/18/13.
+//  Copyright (c) 2013 Charles Joseph Uy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VGCarousel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VGCarousel
+//
+//  Created by Charles Joseph Uy on 9/18/13.
+//  Copyright (c) 2013 Charles Joseph Uy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VGAppDelegate class]));
+    }
+}
