76ea62082326ca45480f2f7c564712091bea670c
Charles Joseph Uy <charles@voyagerinnovation.com>
2013-09-23 10:49:05 +0800

    macro out logging in test view controller

--- VGCarousel/VGTestViewController.m
+++ VGCarousel/VGTestViewController.m
@@ -8,6 +8,8 @@
 
 #import "VGTestViewController.h"
 
+#define NSLOGGING   0
+
 @interface VGTestViewController ()
 
 @end
@@ -30,43 +32,57 @@
 	// Do any additional setup after loading the view.
     UITapGestureRecognizer *doubleTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleDoubleTap:)];
     [self.view addGestureRecognizer:doubleTapGestureRecognizer];
+#if NSLOGGING
     NSLog(@"%@:%@", self.title, NSStringFromSelector(_cmd));
+#endif
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+#if NSLOGGING
     NSLog(@"%@:%@", self.title, NSStringFromSelector(_cmd));
+#endif
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+#if NSLOGGING
     NSLog(@"%@:%@", self.title, NSStringFromSelector(_cmd));
+#endif
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:animated];
+#if NSLOGGING
     NSLog(@"%@:%@", self.title, NSStringFromSelector(_cmd));
+#endif
 }
 
 - (void)viewDidDisappear:(BOOL)animated
 {
     [super viewDidDisappear:animated];
+#if NSLOGGING
     NSLog(@"%@:%@", self.title, NSStringFromSelector(_cmd));
+#endif
 }
 
 - (void)didMoveToParentViewController:(UIViewController *)parent
 {
     [super didMoveToParentViewController:parent];
+#if NSLOGGING
     NSLog(@"%@:%@:%@", self.title, NSStringFromSelector(_cmd), parent);
+#endif
 }
 
 - (void)willMoveToParentViewController:(UIViewController *)parent
 {
     [super willMoveToParentViewController:parent];
+#if NSLOGGING
     NSLog(@"%@:%@:%@", self.title, NSStringFromSelector(_cmd), parent);
+#endif
 }
 
 - (void)didReceiveMemoryWarning
