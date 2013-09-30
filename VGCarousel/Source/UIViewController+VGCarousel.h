c050d9eca249a1d130dd958189bc2a09141cba79
Charles Joseph Uy <charles@voyagerinnovation.com>
2013-09-30 15:28:00 +0800

    add utility method to access parent carousel view controller

---
+++ VGCarousel/Source/UIViewController+VGCarousel.h
@@ -0,0 +1,17 @@
+//
+//  UIViewController+VGCarousel.h
+//  VGCarousel
+//
+//  Created by Charles Joseph Uy on 9/30/13.
+//  Copyright (c) 2013 Charles Joseph Uy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class VGCarouselViewController;
+
+@interface UIViewController (VGCarousel)
+
+- (VGCarouselViewController *)carouselViewController;
+
+@end
---
+++ VGCarousel/Source/UIViewController+VGCarousel.m
@@ -0,0 +1,25 @@
+//
+//  UIViewController+VGCarousel.m
+//  VGCarousel
+//
+//  Created by Charles Joseph Uy on 9/30/13.
+//  Copyright (c) 2013 Charles Joseph Uy. All rights reserved.
+//
+
+#import "UIViewController+VGCarousel.h"
+#import "VGCarouselViewController.h"
+
+@implementation UIViewController (VGCarousel)
+
+- (VGCarouselViewController *)carouselViewController
+{
+    if ([self.parentViewController isKindOfClass:[VGCarouselViewController class]]) {
+        VGCarouselViewController *parent = (VGCarouselViewController *)self.parentViewController;
+        return parent;
+    }
+    else {
+        return nil;
+    }
+}
+
+@end
